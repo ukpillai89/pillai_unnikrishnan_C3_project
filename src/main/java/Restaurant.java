3a7b9a77b70a31ceed7878fbcb6dd486a0c50829
I554440 <unnikrishnan.pillai@sap.com>
2023-07-17 14:54:13 +0530

    Part2:Solution

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,81 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        int openTime = getCurrentTime().compareTo(openingTime);
+        int closeTime = getCurrentTime().compareTo(closingTime);
+        return openTime > 0 && closeTime < 0;
+    }
+
+    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+
+    public List<Item> getMenu() {
+        return menu;
+    }
+
+    private Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public int calculateTotalOrderValue(String[] items){
+//        int total = 0;
+//        for (String item : items){
+//            total += getCostofItem(item);
+//        }
+//        return total;
+//    }
+//
+//    public int getCostofItem(String item){
+//        for (Item i : menu){
+//            if (i.getName().equalsIgnoreCase(item)){
+//                return i.getPrice();
+//            }
+//        }
+//        return 0;
+//    }
+}
